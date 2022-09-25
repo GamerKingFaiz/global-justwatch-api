f1626cf1bef3459df129ab6fe0c2d1c95ca00264
GamerKingFaiz <gamerkingfaiz@gmail.com>
2022-09-25 14:39:15 -0700

    Returning highest season count

--- utils/cleanOffers.js
+++ utils/cleanOffers.js
@@ -4,7 +4,7 @@ const cleanOffers = (offers, country) => {
   /**
    * Only keeping streaming only offers
    */
-  const flatrateOffers = offers.filter(
+  const streamingOffers = offers.filter(
     (offer) =>
       offer.monetization_type === "flatrate" ||
       offer.monetization_type === "free" ||
@@ -15,10 +15,23 @@ const cleanOffers = (offers, country) => {
    * Finds and removes offers with the same package_short_name
    * https://stackoverflow.com/a/36744732/14000052
    */
-  const noDupes = flatrateOffers.filter(
+  let noDupes = streamingOffers.filter(
     (value, index, self) =>
-      index === self.findIndex((t) => t.package_short_name === value.package_short_name)
+      index ===
+      self.findIndex((t) => t.package_short_name === value.package_short_name)
   );
+  // If it's a show return the offer with the highest season count
+  if (offers[0].element_count) {
+    noDupes = noDupes.map((element) => {
+      const singleProviderMultipleQualities = streamingOffers.filter(
+        (offer) => offer.package_short_name === element.package_short_name
+      );
+      // https://stackoverflow.com/a/34087850/14000052
+      return singleProviderMultipleQualities.reduce((prev, current) =>
+        prev.element_count > current.element_count ? prev : current
+      );
+    });
+  }
 
   /**
    * Inputs service's friendly name and icon URL
